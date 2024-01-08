eef8f639b07259d38f4f3f3a095a1910deba683d
Kosuke Tomita <s163404@gmail.com>
2024-01-08 16:39:22 +0900

    050関数と オブジェクト1

--- 050_関数とオブジェクト１/070_bindとthis/end/main.js
+++ 050_関数とオブジェクト１/070_bindとthis/end/main.js
@@ -8,6 +8,9 @@ const person = {
 }
 person.hello();
 
+// bind thisや引数を固定した関数を作成
+// thisキーワードの参照先をbindで指定したpersonに設定
+// person.hello とは別の関数として関数helloがメモリに確保される
 const helloTom = person.hello.bind(person);
 
 function fn(ref) {
@@ -19,7 +22,8 @@ fn(helloTom);
 function a(name) {
     console.log('hello ' + name);
 }
-
+const c = a.bind({name : 'Tim'});
 const b = a.bind(null, 'Tim');
 
 b();
+c();
